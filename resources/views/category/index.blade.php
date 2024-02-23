b3a3969852c08e602e7882a88e53ba18083b8b1c
Jibin-Sample <jibinkjoy3101@gmail.com>
2024-02-23 22:11:23 +0530

    Laravel_10_Simple_CRUD_operation

---
+++ resources/views/category/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layout/app-web-layout')
+@section('content')
+    <div class="container mt-3">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4>Categories</h4>
+                        <a href="" class="btn btn-primary float-end">Add Category</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
